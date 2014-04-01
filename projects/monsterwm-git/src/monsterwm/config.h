8f8d647f15c99fccd7d1153c52038763ce9b8512
Kyle Scott Mcgill <kylescottmcgill@gmail.com>
2014-04-02 09:29:03 +1300

    edit all the things

--- projects/monsterwm-git/src/monsterwm/config.h
+++ projects/monsterwm-git/src/monsterwm/config.h
@@ -17,14 +17,17 @@
 #define DEFAULT_MODE    GRID      /* initial layout/mode: TILE MONOCLE BSTACK GRID FLOAT */
 #define ATTACH_ASIDE    True      /* False means new window is master */
 #define FOLLOW_WINDOW   True      /* follow the window when moved to a different desktop */
+#define FOLLOW_MONITOR  True      /* focus the window the mouse just entered */
 #define FOLLOW_MOUSE    True      /* focus the window the mouse just entered */
 #define CLICK_TO_FOCUS  True      /* focus an unfocused window when clicked  */
 #define FOCUS_BUTTON    Button1   /* mouse button to be used along with CLICK_TO_FOCUS */
 #define BORDER_WIDTH    1         /* window border width */
 #define FOCUS           "#ff950e" /* focused window border color    */
 #define UNFOCUS         "#444444" /* unfocused window border color  */
+#define INFOCUS         "#444444" /* unfocused window border color  */
 #define MINWSZ          50        /* minimum window size in pixels  */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus initially */
+#define DEFAULT_MONITOR 0         /* the desktop to focus initially */
 #define DESKTOPS        4         /* number of desktops - edit DESKTOPCHANGE keys to suit */
 
 /**
@@ -32,9 +35,9 @@
  * if desktop is negative, then current is assumed
  */
 static const AppRule rules[] = { \
-    /*  class     desktop  follow  float */
-    { "chromium",   1,    True,   False },
-    { "Mplayer",    0,    False,  True  },
+    /*  class       monitor   desktop   follow      float */
+    { "chromium",   1,        0,        True,       False },
+    { "Mplayer",    0,        0,        False,      True  },
 };
 
 /* helper for spawning shell commands */
@@ -47,17 +50,21 @@ static const AppRule rules[] = { \
 static const char *termcmd[] = { "termite",   NULL };
 static const char *menucmd[] = { "dmenu_run", NULL };
 
+#define MONITORCHANGE(K,N) \
+    {  MOD4,             K,              change_monitor, {.i = N}}, \
+    {  MOD4|ShiftMask,   K,              client_to_monitor, {.i = N}},
+
 #define DESKTOPCHANGE(K,N) \
-    {  MOD1,             K,              change_desktop, {.i = N}}, \
-    {  MOD1|ShiftMask,   K,              client_to_desktop, {.i = N}},
+    {  MOD4,             K,              change_desktop, {.i = N}}, \
+    {  MOD4|ShiftMask,   K,              client_to_desktop, {.i = N}},
 
 /**
  * keyboard shortcuts
  */
 static Key keys[] = {
     /* modifier          key            function           argument */
-    {  MOD1,             XK_b,          togglepanel,       {NULL}},
-    {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
+    // {  MOD1,             XK_b,          togglepanel,       {NULL}},
+    // {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
     {  MOD1|SHIFT,       XK_c,          killclient,        {NULL}},
     {  MOD1,             XK_j,          next_win,          {NULL}},
     {  MOD1,             XK_k,          prev_win,          {NULL}},
@@ -65,11 +72,11 @@ static Key keys[] = {
     {  MOD1,             XK_l,          resize_master,     {.i = +10}}, /* increase size in px */
     {  MOD1,             XK_o,          resize_stack,      {.i = -10}}, /* shrink   size in px */
     {  MOD1,             XK_p,          resize_stack,      {.i = +10}}, /* grow     size in px */
-    {  MOD1|CONTROL,     XK_h,          rotate,            {.i = -1}},
-    {  MOD1|CONTROL,     XK_l,          rotate,            {.i = +1}},
-    {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = -1}},
-    {  MOD1|SHIFT,       XK_l,          rotate_filled,     {.i = +1}},
-    {  MOD1,             XK_Tab,        last_desktop,      {NULL}},
+    // {  MOD1|CONTROL,     XK_h,          rotate,            {.i = -1}},
+    // {  MOD1|CONTROL,     XK_l,          rotate,            {.i = +1}},
+    // {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = -1}},
+    // {  MOD1|SHIFT,       XK_l,          rotate_filled,     {.i = +1}},
+    {  MOD4,             XK_Tab,        last_desktop,      {NULL}},
     {  MOD1,             XK_Return,     swap_master,       {NULL}},
     {  MOD1|SHIFT,       XK_j,          move_down,         {NULL}},
     {  MOD1|SHIFT,       XK_k,          move_up,           {NULL}},
@@ -81,7 +88,7 @@ static Key keys[] = {
     {  MOD1|CONTROL,     XK_r,          quit,              {.i = 0}}, /* quit with exit value 0 */
     {  MOD1|CONTROL,     XK_q,          quit,              {.i = 1}}, /* quit with exit value 1 */
     {  MOD4,             XK_Return,     spawn,             {.com = termcmd}},
-    {  MOD4,             XK_p,          spawn,             {.com = menucmd}},
+    {  MOD4,             XK_grave,      spawn,             {.com = menucmd}},
     {  MOD4,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
     {  MOD4,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
     {  MOD4,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
@@ -90,10 +97,12 @@ static Key keys[] = {
     {  MOD4|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
     {  MOD4|SHIFT,       XK_l,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
     {  MOD4|SHIFT,       XK_h,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
-       DESKTOPCHANGE(    XK_F1,                             0)
-       DESKTOPCHANGE(    XK_F2,                             1)
-       DESKTOPCHANGE(    XK_F3,                             2)
-       DESKTOPCHANGE(    XK_F4,                             3)
+       DESKTOPCHANGE(    XK_1,                             0)
+       DESKTOPCHANGE(    XK_2,                             1)
+       DESKTOPCHANGE(    XK_3,                             2)
+       DESKTOPCHANGE(    XK_4,                             3)
+       MONITORCHANGE(    XK_e,                             1)
+       MONITORCHANGE(    XK_r,                             0)
 };
 
 /**
