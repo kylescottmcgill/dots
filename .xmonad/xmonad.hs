d96391c08b6f99bb8bb5ee966f2fdb20f6de7493
Kyle Scott Mcgill <email@kylescottmcgill.com>
2015-03-02 13:49:05 +1300

    meh

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -31,7 +31,7 @@ import System.IO
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal		= "urxvt"
+myTerminal = "urxvtc"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
@@ -43,14 +43,14 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth	= 1
+myBorderWidth = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
-myModMask		= mod4Mask
+myModMask = mod4Mask
 
 -- The default number of workspaces (virtual screens) and their names.
 -- By default we use numeric strings, but any string may be used as a
@@ -61,11 +61,11 @@ myModMask		= mod4Mask
 --
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
-myWorkspaces	= ["term", "web", "dev"] ++ map show [4..9]
+myWorkspaces = ["term", "web", "dev"] ++ map show [4..9]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#272935"
+myNormalBorderColor = "#272935"
 myFocusedBorderColor = "#cc6666"
 
 ------------------------------------------------------------------------
@@ -74,109 +74,102 @@ myFocusedBorderColor = "#cc6666"
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
 -- launch a terminal
-[
-((modm, xK_Return), spawn $ XMonad.terminal conf),
+	[ ((modm, xK_Return), spawn $ XMonad.terminal conf),
 
 -- launch rofi
-((modm .|. shiftMask, xK_grave), runOrRaisePrompt mySP),
-((modm, xK_grave), spawn "rofi -show run"),
+	((modm .|. shiftMask, xK_grave), runOrRaisePrompt mySP),
+	((modm, xK_grave), spawn "rofi -show run"),
 
 -- close focused window
-((modm, xK_q), kill),
+	((modm, xK_q), kill),
 
 -- Rotate through the available layout algorithms
-((modm, xK_space), sendMessage NextLayout),
+	((modm, xK_space), sendMessage NextLayout),
 
 --	Reset the layouts on the current workspace to default
-((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf),
+	((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf),
 
 -- Resize viewed windows to the correct size
-((modm, xK_n), refresh),
+	((modm, xK_n), refresh),
 
 -- Move focus to the next window
-((modm, xK_Tab), windows W.focusDown),
+	((modm, xK_Tab), windows W.focusDown),
 
 -- Move focus to the next window
-((modm, xK_j), windows W.focusDown),
+	((modm, xK_j), windows W.focusDown),
 
 -- Move focus to the previous window
-((modm, xK_k), windows W.focusUp),
+	((modm, xK_k), windows W.focusUp),
 
 -- Move focus to the master window
-((modm, xK_m), windows W.focusMaster),
+	((modm, xK_m), windows W.focusMaster),
 
 -- Swap the focused window and the master window
-((modm .|. shiftMask, xK_Return), windows W.swapMaster),
+	((modm .|. shiftMask, xK_Return), windows W.swapMaster),
 
 -- Swap the focused window with the next window
-((modm .|. shiftMask, xK_j), windows W.swapDown),
+	((modm .|. shiftMask, xK_j), windows W.swapDown),
 
 -- Swap the focused window with the previous window
-((modm .|. shiftMask, xK_k), windows W.swapUp),
+	((modm .|. shiftMask, xK_k), windows W.swapUp),
 
 -- Layout
-((modm, xK_a), sendMessage Shrink),
-((modm, xK_s), sendMessage Expand),
-((modm .|. shiftMask, xK_a), sendMessage MirrorShrink),
-((modm .|. shiftMask, xK_s), sendMessage MirrorExpand),
+	((modm, xK_a), sendMessage Shrink),
+	((modm, xK_s), sendMessage Expand),
+	((modm .|. shiftMask, xK_a), sendMessage MirrorShrink),
+	((modm .|. shiftMask, xK_s), sendMessage MirrorExpand),
 
 -- Push window back into tiling
-((modm, xK_t	   ), withFocused $ windows . W.sink),
+	((modm, xK_t	   ), withFocused $ windows . W.sink),
 
 -- Increment the number of windows in the master area
-((modm, xK_comma ), sendMessage (IncMasterN 1)),
+	((modm, xK_comma ), sendMessage (IncMasterN 1)),
 
 -- Deincrement the number of windows in the master area
-((modm, xK_period), sendMessage (IncMasterN (-1))),
+	((modm, xK_period), sendMessage (IncMasterN (-1))),
 
 -- Toggle the status bar gap
 -- Use this binding with avoidStruts from Hooks.ManageDocks.
 -- See also the statusBar function from Hooks.DynamicLog.
 --
-((modm, xK_x), sendMessage ToggleStruts),
+	((modm, xK_x), sendMessage ToggleStruts),
 
 -- Restart xmonad
-((modm .|. shiftMask,  xK_q), spawn "xmonad --recompile; xmonad --restart ; rm ~/.xmonad/xmonad.{o,errors,hi}"),
+	((modm .|. shiftMask,  xK_q), spawn "xmonad --recompile; xmonad --restart ; rm ~/.xmonad/xmonad.{o,errors,hi}"),
 
 -- Launch browser
-((modm, xK_b), runOrRaise "chromium" (className =? "chromium")),
+	((modm, xK_b), runOrRaise "chromium" (className =? "chromium")),
 
-((0, xF86XK_AudioPlay), spawn "ncmpcpp toggle"),
-((0, xF86XK_AudioNext), spawn "ncmpcpp next"),
-((0, xF86XK_AudioPrev), spawn "ncmpcpp prev"),
-((0, xF86XK_AudioLowerVolume), spawn "amixer -c 0 set Master 5dB-"),
-((0, xF86XK_AudioRaiseVolume), spawn "amixer -c 0 set Master 5dB+"),
-((modm .|. altMask, xK_l), sendMessage $ ExpandTowards R),
-((modm .|. altMask, xK_h), sendMessage $ ExpandTowards L),
-((modm .|. altMask, xK_j), sendMessage $ ExpandTowards D),
-((modm .|. altMask, xK_k), sendMessage $ ExpandTowards U),
-((modm .|. altMask .|. ctrlMask , xK_l), sendMessage $ ShrinkFrom R),
-((modm .|. altMask .|. ctrlMask , xK_h), sendMessage $ ShrinkFrom L),
-((modm .|. altMask .|. ctrlMask , xK_j), sendMessage $ ShrinkFrom D),
-((modm .|. altMask .|. ctrlMask , xK_k), sendMessage $ ShrinkFrom U),
-((modm, xK_r), sendMessage Rotate),
-((modm, xK_s), sendMessage Swap)
-
-]
-++
+	((0, xF86XK_AudioPlay), spawn "ncmpcpp toggle"),
+	((0, xF86XK_AudioNext), spawn "ncmpcpp next"),
+	((0, xF86XK_AudioPrev), spawn "ncmpcpp prev"),
+	((0, xF86XK_AudioLowerVolume), spawn "amixer -c 0 set Master 5dB-"),
+	((0, xF86XK_AudioRaiseVolume), spawn "amixer -c 0 set Master 5dB+"),
+	((modm, xK_l), sendMessage $ ExpandTowards R),
+	((modm, xK_h), sendMessage $ ExpandTowards L),
+	((modm, xK_j), sendMessage $ ExpandTowards D),
+	((modm, xK_k), sendMessage $ ExpandTowards U),
+	((modm .|. shiftMask , xK_l), sendMessage $ ShrinkFrom R),
+	((modm .|. shiftMask , xK_h), sendMessage $ ShrinkFrom L),
+	((modm .|. shiftMask , xK_j), sendMessage $ ShrinkFrom D),
+	((modm .|. shiftMask , xK_k), sendMessage $ ShrinkFrom U),
+	((modm, xK_r), sendMessage Rotate),
+	((modm, xK_s), sendMessage Swap)
+	]
+	++
 
 --
 -- mod-[1..9], Switch to workspace N
 -- mod-shift-[1..9], Move client to workspace N
 --
-[((m .|. modm, k), windows $ f i)
-| (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-										 , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-										 ++
+	[((m .|. modm, k), windows $ f i) | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9], (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+	++
 
---
--- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
--- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
---
-[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-| (key, sc) <- zip [xK_w, xK_e, xK_r] [2,1,0]
-				   , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-
+	--
+	-- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+	-- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+	--
+	[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f)) | (key, sc) <- zip [xK_w, xK_e, xK_r] [2,1,0], (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
@@ -184,16 +177,16 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
 -- mod-button1, Set the window to floating mode and move by dragging
-[ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
+	[ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
 
--- mod-button2, Raise the window to the top of the stack
-, ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+	-- mod-button2, Raise the window to the top of the stack
+	, ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
 
--- mod-button3, Set the window to floating mode and resize by dragging
-, ((modm, button3), (\w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster))
+	-- mod-button3, Set the window to floating mode and resize by dragging
+	, ((modm, button3), (\w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster))
 
--- you may also bind events to the mouse scroll wheel (button4 and button5)
-]
+	-- you may also bind events to the mouse scroll wheel (button4 and button5)
+	]
 
 ------------------------------------------------------------------------
 -- Layouts:
@@ -212,16 +205,16 @@ myLayout = avoidStruts $ spacing 2 $ emptyBSP ||| Grid ||| full ||| tiled ||| Mi
 		-- tiled = Tall nmaster delta ratio
 		tiled = ResizableTall (1) (3/100) (1/2) []
 
--- The default number of windows in the master pane
-nmaster = 1
+		-- The default number of windows in the master pane
+		nmaster = 1
 
--- Default proportion of screen occupied by master pane
-ratio = 1/2
+		-- Default proportion of screen occupied by master pane
+		ratio = 1/2
 
--- Percent of screen to increment by when resizing panes
-delta = 3/100
+		-- Percent of screen to increment by when resizing panes
+		delta = 3/100
 
-full = noBorders Full
+		full = noBorders Full
 
 ------------------------------------------------------------------------
 -- Window rules:
@@ -239,10 +232,10 @@ full = noBorders Full
 -- 'className' and 'resource' are used below.
 --
 myManageHook = composeAll
-[ className =? "MPlayer"		--> doFloat
-, className =? "Gimp"			--> doFloat
-, resource	=? "desktop_window" --> doIgnore
-, resource	=? "kdesktop"		--> doIgnore ]
+	[ className =? "MPlayer"		--> doFloat
+	, className =? "Gimp"			--> doFloat
+	, resource	=? "desktop_window" --> doIgnore
+	, resource	=? "kdesktop"		--> doIgnore ]
 
 ------------------------------------------------------------------------
 -- Event handling
