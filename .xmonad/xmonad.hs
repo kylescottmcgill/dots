17a41b50fdfaa3510bb75d1efb1fc3aec64aa26e
Kyle Scott Mcgill <email@kylescottmcgill.com>
2015-03-02 13:56:08 +1300

    ws

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -205,16 +205,12 @@ myLayout = avoidStruts $ spacing 2 $ emptyBSP ||| Grid ||| full ||| tiled ||| Mi
 		-- default tiling algorithm partitions the screen into two panes
 		-- tiled = Tall nmaster delta ratio
 		tiled = ResizableTall (1) (3/100) (1/2) []
-
 		-- The default number of windows in the master pane
 		nmaster = 1
-
 		-- Default proportion of screen occupied by master pane
 		ratio = 1/2
-
 		-- Percent of screen to increment by when resizing panes
 		delta = 3/100
-
 		full = noBorders Full
 
 ------------------------------------------------------------------------
