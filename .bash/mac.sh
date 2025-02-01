b1057dada7dff55308e1b3db4299b39f4819dc30
Kyle Scott Mcgill <email@kylescottmcgill.com>
2025-02-02 12:48:59 +1300

    minor

--- .bash/mac.sh
+++ .bash/mac.sh
@@ -3,3 +3,20 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 export HOMEBREW_NO_ENV_HINTS=1
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
+if [ "$ALACRITTY" = "true" ]
+then
+  theme() {
+    ln -sf $HOME/.config/alacritty/themes/themes/$1.toml $HOME/.config/alacritty/active-theme.toml
+  }
+
+  ALACRITTY_THEME=$(defaults read -g AppleInterfaceStyle 2>/dev/null || echo "Light")
+
+  if [ "$ALACRITTY_THEME" = "Dark" ]
+  then
+    theme "pencil_dark"
+  else
+    theme "pencil_light"
+  fi
+fi
